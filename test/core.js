fee2c86ef6a5ebb73d254a0de65793d16b7e2b2e
manlio <mr-spazzaneve@live.it>
2023-12-11 23:15:20 +0000

    .

--- test/core.js
+++ test/core.js
@@ -5,7 +5,7 @@ function main() {
     
 	var s;
 
-    var contents = fetch('file.txt')
+    var contents = fetch('file.txt', {cache: "no-store"})
 	.then((res) => res.text())
 	.then((text) => {
 		papers.innerHTML = text;
