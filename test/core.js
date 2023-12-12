c48b257e08806056a62b98e9f1f467796096fb89
manlio <mr-spazzaneve@live.it>
2023-12-12 00:42:54 +0000

    .

--- test/core.js
+++ test/core.js
@@ -3,17 +3,23 @@ var papers = document.getElementById("papers");
 
 function main() {
     
-	var s;
-
     var contents = fetch('file.txt', {cache: "no-cache"})
 	.then((res) => res.text())
 	.then((text) => {
 		papers.innerHTML = text;
 	   }).catch((e) => console.error(e));
 
-    var fm = document.getElementById("fmath");
-    fm.innerHTML = '$\\sqrt{2}$';
-
+   	
+	   var fm = document.getElementById("fmath");
+	   fm.innerHTML = '$\\sqrt{2}$';
+   
+	   var contents = fetch('file2.txt', {cache: "no-cache"})
+	   .then((res) => res.text())
+	   .then((text) => {
+		var p2 = document.getElementById("p2");
+		   p2.innerHTML = text;
+		  }).catch((e) => console.error(e));
+   
 	// MathJax.typeset();
 }
 
